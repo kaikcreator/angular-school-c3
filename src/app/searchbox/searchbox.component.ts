caa5bb838d0282f38af2f79f4dfb2773f43b2cc0
Enrique Oriol <enrique.oriol@dynatrace.com>
2020-02-14 16:55:47 +0100

    Updated to angular 9

--- src/app/searchbox/searchbox.component.ts
+++ src/app/searchbox/searchbox.component.ts
@@ -7,7 +7,7 @@ import { NgModel } from '@angular/forms';
   styleUrls: ['./searchbox.component.scss']
 })
 export class SearchboxComponent implements OnInit {
-  @ViewChild('searchBox') public searchBox:NgModel;
+  @ViewChild('searchBox', { static: true }) public searchBox:NgModel;
   @Output() value: EventEmitter<string> = new EventEmitter();
   public text:string = '';
   
